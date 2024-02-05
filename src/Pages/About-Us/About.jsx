9c62dd660196fc94280d83ecd2a386f4f2d8d3de
rahulbera9386 <rahulberastd2002@gmail.com>
2024-02-05 10:49:16 +0530

    Added navbar & pages to project

---
+++ src/Pages/About-Us/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./About.css"
+const About= () => {
+  return (
+    <div>About-Us</div>
+  )
+}
+
+export default About;
---
+++ src/Pages/Error/error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Error.css"
+const error = () => {
+  return (
+    <div>error</div>
+  )
+}
+
+export default error
