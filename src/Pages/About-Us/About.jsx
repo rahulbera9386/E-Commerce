9475f2acf4702c27d909792743346200c84fbbfc
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-03-16 21:39:20 +0530

    Created an Error Page

--- src/Pages/About-Us/About.jsx
+++ src/Pages/About-Us/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import "./About.css"
 const About= () => {
   return (
-    <div>About-Us</div>
+    <>
+     <h1 className="text-3xl font-bold ">
+      About Us
+    </h1>
+    </>
   )
 }
 
